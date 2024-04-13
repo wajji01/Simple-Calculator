e42bc7f4a5b549bea9cbec9451f31946a238fa22
wajji01 <wajahatmustafa725@gmail.com>
2024-04-13 13:31:54 +0500

    applying while loop

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+let condition = true;
 console.log("===============================");
 console.log("      Wajahat Calculator       ");
 console.log("===============================");
-const answer = await inquirer.prompt([
-    { message: "Enter your first number", type: "number", name: "firstnum" },
-    { message: "Enter your second number", type: "number", name: "secondnum" },
-    {
-        message: "Enter a valid operator",
-        type: "list",
-        name: "operator",
-        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
-    },
-]);
-// conditional statment
-if (answer.operator === "Addition") {
-    console.log("Addition of first and second number is " + (answer.firstnum + answer.secondnum));
-}
-else if (answer.operator === "Subtraction") {
-    console.log("Subtraction of first and second number is " + (answer.firstnum - answer.secondnum));
-}
-else if (answer.operator === "Multiplication") {
-    console.log("Multiplication of first and second number is " + (answer.firstnum * answer.secondnum));
-}
-else if (answer.operator === "Division") {
-    console.log("Division of first and second number is " + (answer.firstnum / answer.secondnum));
-}
-else {
-    console.log("Please select a valid operator");
+while (condition) {
+    const answer = await inquirer.prompt([
+        { message: "Enter your first number", type: "number", name: "firstnum" },
+        { message: "Enter your second number", type: "number", name: "secondnum" },
+        {
+            message: "Enter a valid operator",
+            type: "list",
+            name: "operator",
+            choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+        },
+    ]);
+    // conditional statment
+    if (answer.operator === "Addition") {
+        console.log("Addition of first and second number is " +
+            (answer.firstnum + answer.secondnum));
+    }
+    else if (answer.operator === "Subtraction") {
+        console.log("Subtraction of first and second number is " +
+            (answer.firstnum - answer.secondnum));
+    }
+    else if (answer.operator === "Multiplication") {
+        console.log("Multiplication of first and second number is " +
+            answer.firstnum * answer.secondnum);
+    }
+    else if (answer.operator === "Division") {
+        console.log("Division of first and second number is " +
+            answer.firstnum / answer.secondnum);
+    }
+    else {
+        console.log("Please select a valid operator");
+    }
+    let cond = await inquirer.prompt([
+        {
+            name: "more",
+            type: "confirm",
+            message: "Do you want do more ?",
+            default: "false",
+        }
+    ]);
+    condition = cond.more;
 }
 console.log("===============================");
 console.log("      Thanks For Comming       ");
--- index.ts
+++ index.ts
@@ -1,36 +1,61 @@
 #! /usr/bin/env node
-
 import inquirer from "inquirer";
+let condition = true;
 
-console.log("===============================")
-console.log("      Wajahat Calculator       ")
-console.log("===============================")
+console.log("===============================");
+console.log("      Wajahat Calculator       ");
+console.log("===============================");
+while (condition) {
 
-const answer = await inquirer.prompt([
-  { message: "Enter your first number", type: "number", name: "firstnum" },
-  { message: "Enter your second number", type: "number", name: "secondnum" },
-  {
-    message: "Enter a valid operator",
-    type: "list",
-    name: "operator",
-    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
-  },
-]);
-
-// conditional statment
-
-if (answer.operator === "Addition"){
-    console.log("Addition of first and second number is " + (answer.firstnum + answer.secondnum));
-} else if (answer.operator === "Subtraction"){
-    console.log("Subtraction of first and second number is " + (answer.firstnum - answer.secondnum));
-} else if (answer.operator === "Multiplication"){
-    console.log("Multiplication of first and second number is " + (answer.firstnum * answer.secondnum));
-} else if (answer.operator === "Division"){
-    console.log("Division of first and second number is " + (answer.firstnum / answer.secondnum));
-} else {
-    console.log("Please select a valid operator")
-}
-
-console.log("===============================")
-console.log("      Thanks For Comming       ")
-console.log("===============================")
+  const answer = await inquirer.prompt([
+    { message: "Enter your first number", type: "number", name: "firstnum" },
+    { message: "Enter your second number", type: "number", name: "secondnum" },
+    {
+      message: "Enter a valid operator",
+      type: "list",
+      name: "operator",
+      choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+   
+  ]);
+  // conditional statment
+  if (answer.operator === "Addition") {
+    console.log(
+      "Addition of first and second number is " +
+        (answer.firstnum + answer.secondnum)
+    );
+  } else if (answer.operator === "Subtraction") {
+    console.log(
+      "Subtraction of first and second number is " +
+        (answer.firstnum - answer.secondnum)
+    );
+  } else if (answer.operator === "Multiplication") {
+    console.log(
+      "Multiplication of first and second number is " +
+        answer.firstnum * answer.secondnum
+    );
+  } else if (answer.operator === "Division") {
+    console.log(
+      "Division of first and second number is " +
+        answer.firstnum / answer.secondnum
+    );
+  } else {
+    console.log("Please select a valid operator");
+  }
+  
+  let cond = await inquirer.prompt(
+      [
+          {
+              name: "more",
+              type: "confirm",
+              message: "Do you want do more ?",
+              default: "false",
+            }
+        ]
+        )
+        condition = cond.more;
+    }
+    
+    console.log("===============================");
+    console.log("      Thanks For Comming       ");
+    console.log("===============================");
